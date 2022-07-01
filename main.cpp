683d519564812f1b0fdd22cec2f244ff46b5d2a4
ThapeloSeletisha <thapeloseletisha01@gmail.com>
2022-07-01 21:22:28 +0200

    defined interfaces

--- main.cpp
+++ main.cpp
@@ -5,35 +5,73 @@
 #include <algorithm>
 using namespace std;
 
-class Person{};
+class Person
+{
+private:
+    int age, cur_id;
+    string name;
 
-class Professor : public Person{};
+public: 
+    Person(){};
+    virtual void getData() = 0;
+    virtual void putData() = 0;
+};
 
-class Student : public Person{};
+class Professor : public Person
+{
+private:
+    int publications;
+    static int idGenerator;
 
-int main(){
+public:
 
-    int n, val;
-    cin>>n; //The number of objects that is going to be created.
-    Person *per[n];
+    void getData();
+    void putData();
+};
 
-    for(int i = 0;i < n;i++){
+int Professor::idGenerator = 1;
 
-        cin>>val;
-        if(val == 1){
-            // If val is 1 current object is of type Professor
-            per[i] = new Professor;
+class Student : public Person
+{
+private:
+    vector<int> marks;
+    static int idGenerator;
 
-        }
-        else per[i] = new Student; // Else the current object is of type Student
+public:
+    void getData();
+    void putData();
+};
 
-        per[i]->getdata(); // Get the data from the user.
+int Student::idGenerator = 1;
 
-    }
-
-    for(int i=0;i<n;i++)
-        per[i]->putdata(); // Print the required output for each object.
-
+int main()
+{
     return 0;
-
 }
+
+// int main(){
+
+//     int n, val;
+//     cin>>n; //The number of objects that is going to be created.
+//     Person *per[n];
+
+//     for(int i = 0;i < n;i++){
+
+//         cin>>val;
+//         if(val == 1){
+//             // If val is 1 current object is of type Professor
+//             per[i] = new Professor;
+
+//         }
+//         else per[i] = new Student; // Else the current object is of type Student
+
+//         per[i]->getdata(); // Get the data from the user.
+
+//     }
+
+//     for(int i=0;i<n;i++)
+//         per[i]->putdata(); // Print the required output for each object.
+
+//     return 0;
+
+// }
