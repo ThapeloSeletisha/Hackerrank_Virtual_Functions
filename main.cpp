5a37899d4c0b93ce68076bb8f3482e532d3ecb10
ThapeloSeletisha <thapeloseletisha01@gmail.com>
2022-07-01 21:50:57 +0200

    defined constructors & test main

--- main.cpp
+++ main.cpp
@@ -1,21 +1,29 @@
 #include <cmath>
 #include <cstdio>
 #include <vector>
+#include <array>
 #include <iostream>
 #include <algorithm>
 using namespace std;
 
 class Person
 {
-private:
+protected:
     int age, cur_id;
     string name;
 
 public: 
-    Person(){};
+    Person();
+    // virtual ~Person() = 0;
     virtual void getData() = 0;
     virtual void putData() = 0;
 };
+
+Person::Person()
+{
+    age = cur_id = 0;
+    name = "";
+}
 
 class Professor : public Person
 {
@@ -24,28 +32,79 @@ private:
     static int idGenerator;
 
 public:
-
+    Professor();
     void getData();
     void putData();
+
+    static int generateID();
+
 };
 
 int Professor::idGenerator = 1;
 
+int Professor::generateID()
+{
+    return idGenerator++;
+}
+
+Professor::Professor() : Person()
+{
+    publications = 0;
+    cur_id = Professor::generateID();
+}
+
+void Professor::getData()
+{
+
+}
+
+void Professor::putData()
+{
+
+}
+
 class Student : public Person
 {
 private:
-    vector<int> marks;
+    array<int, 6> marks;
     static int idGenerator;
 
 public:
+    Student();
     void getData();
     void putData();
+
+    static int generateID();
 };
 
 int Student::idGenerator = 1;
 
+int Student::generateID()
+{
+    return idGenerator++;
+}
+
+Student::Student() : Person()
+{
+    cur_id = Student::generateID();
+}
+
+void Student::getData()
+{
+
+}
+
+void Student::putData()
+{
+    
+}
+
 int main()
 {
+    Person *per[3];
+    per[0] = new Professor();
+    per[1] = new Professor();
+    per[2] = new Student();
     return 0;
 }
 
